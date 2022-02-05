21638f4bb1a8bb18b25a70e621c5ea27f0c01a53
Paul Jarvis <paulj@raukawa.gns.cri.nz>
2022-02-06 00:56:54 +1300

    Forked branch from Larry. Created folder to contain scripts used to make input files.

---
+++ makeInput/HTHH.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Bash script to create a set of input files for a Monte-Carlo sweep over input
+# parameters so that Plumeria can be used to model the HTHH 15th January 2022
+# eruption
+
+# User-defined variables #######################################################
+
+# Directory to store input files
+fileDir='/home/paulj/Documents/tonga2022/plumeria_wd/input/HTHH/'
+
+# Number of simulations to be performed
+nSims=500
+
+# 
---
+++ makeInput/readme.txt
@@ -0,0 +1,4 @@
+README file created by Paul Jarvis
+
+This directory contains scripts used to automate the creation of input files
+for specific case studies.
